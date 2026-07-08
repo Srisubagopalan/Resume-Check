0376eabdc38771687df360dbbe9a1f144ed2a8d3
Srisubagopalan <sribalaammuanand@gmail.com>
2026-07-08 17:52:40 +0530

    Update settings.py

--- resume_analyzer/resume_analyzer/settings.py
+++ resume_analyzer/resume_analyzer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k7ufdt#hwh3esl_xvr=#u50hsx&w+p3f6twak)zvkjzhqof!7='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["+"]
 
