fb98f7d6c652611013189551372f7ff8da1595f2
Srisubagopalan <sribalaammuanand@gmail.com>
2026-07-08 17:53:01 +0530

    Update settings.py

--- resume_analyzer/resume_analyzer/settings.py
+++ resume_analyzer/resume_analyzer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k7ufdt#hwh3esl_xvr=#u50hsx&w+p3f6twak)zvkjzhqof!7=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["+"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
